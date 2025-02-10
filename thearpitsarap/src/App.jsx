30d84a90f4d8ae1af01363768135296387b67fff
thearpitsarap <arpitsarap18@gmail.com>
2025-02-10 16:44:05 +0530

    initial setup

---
+++ thearpitsarap/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex justify-center'>
+      <div className='font-semibold'>
+        testting
+      </div>
+    </div>
+  )
+}
+
+export default App
